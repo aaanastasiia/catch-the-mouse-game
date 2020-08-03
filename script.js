e22ac4f81b48f6ada3fb437e05f619672afd37e3
Анищик Анастасия <an.anischik@gmail.com>
2020-08-03 10:17:11 +0300

    added changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+  
 class game {
     constructor() {
         this.score = 0;
@@ -6,15 +7,16 @@ class game {
         this.isMouse = false;
         this.level = 0;
         this.pickedEllipse = null;
-        this.timer = 1300;
+        this.timer = 1600;
         this.pointsCounter = 0;
         this.speedCounter = 1;
         this.emojiType = '';
         this.lifeCounter = 0;
-        // this.createInterval = 2000;
+        this.emojiWrap = '';
+        this.interval = '';
 
         this.sumAnimals = {
-            mouse: 10,
+            mouse: 8,
             panda: 1,
             bear: 1,
             fox: 1,
@@ -44,7 +46,6 @@ class game {
         this.emojies = [];
 
         this.ellipse = document.querySelectorAll('.ellipse');
-        this.emojiWrap = document.createElement('div');
         this.speed = document.getElementById('speed');
         this.points = document.getElementById('points');
         this.star = document.querySelector('.star__icon');
@@ -56,7 +57,7 @@ class game {
 
     createEmojiesArr() {
         for (const key in this.sumAnimals) {
-            for(let i = this.sumAnimals[key]; i > 0; i--){
+            for(let i = this.sumAnimals[key]; i > 0; i--) {
                 this.emojies.push(this.animals[key]);
             }
         }
@@ -64,62 +65,78 @@ class game {
 
     deleteElement() {
         let deleteEmoji = document.querySelector('.wrap__emoji');
-        if (deleteEmoji) 
-        deleteEmoji.remove();
-        this.isMouse = false;   
+        if (deleteEmoji) {
+            deleteEmoji.remove();
+        }
+        this.isMouse = false;  
     }
 
     getRandomEmoji() {
         let emojiRandom = (Math.round(Math.random() * (this.emojies.length - 1)));
-        console.log(this.emojies[emojiRandom]);
         if (this.emojies[emojiRandom] === '🐭') {
             this.isMouse = true; 
         }
         return this.emojies[emojiRandom];
-        
     }
 
     createEmoji() { 
         this.deleteElement();
+        this.emojiWrap = document.createElement('div');
         let ellipseRandom = (Math.round(Math.random() * (this.ellipse.length - 1)));
         this.emojiWrap.classList.add('wrap__emoji');
         this.ellipse[ellipseRandom].appendChild(this.emojiWrap);
         this.emojiWrap.innerHTML = this.getRandomEmoji();
-        this.emojiWrap.addEventListener('click', () => this.clickEmoji(), {once: true});
+        this.emojiWrap.addEventListener('click', () => this.clickEmoji());
     }
 
-    clickEmoji() {   
-        this.speedCounter = 1;
+    clickEmoji() {
+        this.timer = 1200;
         if (this.isMouse) {
-            this.pointsCounter += 10; 
             this.star.style.animation = 'none';
+            this.pointsCounter += 10; 
                 if (this.pointsCounter % 50 === 0) {
                     this.star.style.animation = 'flash 1s ease-in-out';
                     this.speedCounter += 1;
-                    // setTimeout(() => { this.speedCounter += 1;
-                    // speed.innerHTML = this.speedCounter
-                    // }, this.timer * 0.9); 
+                    // this.timer = 1600 - (300 * this.speedCounter);
+                    console.log(this.speedCounter);
+                    this.createTimer();
                 }
         }
-        else {        
+        else {
             this.health--;
-            console.log(this.health);
             this.lifeCounter++;
             const livesCounter = 3;
-            console.log(this.lives[livesCounter-this.lifeCounter]);
             this.lives[livesCounter-this.lifeCounter].style.display = 'none';
             this.death[livesCounter-this.lifeCounter].style.display = 'block';
         }
         if (this.health === 0) {
-            this.deleteElement();
             clearInterval(this.interval);
             const counter = document.getElementById('points-counter');
             counter.innerHTML = this.pointsCounter;
             this.popupGameOver.classList.add('active');
             this.endGame();
         }
+        // this.interval = setInterval( () => this.createEmoji(), this.timer);
+        this.deleteElement();
+        this.isMouse = false;
         this.setPoints();
         this.setScore();
+    }
+
+    createTimer() {
+        console.log(this.timer);
+        if (this.pointsCounter % 50 === 0) {
+       this.timer = 1300 - (300 * this.speedCounter);
+        }
+        else {
+            this.timer;
+        }
+        console.log(this.timer);
+    }
+
+    healthFull() {
+        this.lives.forEach((live) => live.style.display = 'block');
+        this.death.forEach((item) => item.style.display = 'none');
     }
 
     setPoints() {
@@ -133,23 +150,29 @@ class game {
     startGame() {
         this.isRunning = true;
         this.createEmojiesArr ();
-        this.createEmoji ();
         this.setPoints ();
-        this.setScore()
-        this.interval = setInterval( () => this.createEmoji(),this.timer);
+        this.setScore();
+        this.createTimer();
+        this.interval = setInterval( () => this.createEmoji(), this.timer);
     }
 
     endGame() {
+        this.healthFull();
         this.health = 3;
-        this.points = 0;
+        this.pointsCounter = 0;
+        this.speedCounter = 0;
+        this.speed.innerHTML = this.speedCounter;
         this.points.innerHTML = this.pointsCounter;
-        if (this.btnGameOver !== null) {
         this.btnGameOver.addEventListener('click', () => {this.popupGameOver.classList.remove('active')});
-        }
     }
-
-    
 }
+
 let start = document.querySelector('.start__button');
 start.addEventListener('click', () => { let newGame = new game();
-                                        newGame.startGame();});
+                                        newGame.startGame()});
+
+const rulesBtn = document.querySelector('.question__button');
+const acceptRules = document.querySelector('.popup__accept_btn');
+const popup = document.querySelector('.popup');
+rulesBtn.addEventListener('click', () => popup.style.display = 'block');
+acceptRules.addEventListener('click', () => popup.style.display = 'none');
